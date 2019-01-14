9935d098999f1c12adf18d04b06b8c7e89c4c8fa
Eunjin Kim <jagpavv@gmail.com>
2019-01-14 10:57:59 +0100

    Add suggestion buttons

--- Suggestions/ViewController.swift
+++ Suggestions/ViewController.swift
@@ -3,20 +3,68 @@ import ActionSheetPicker_3_0
 
 class ViewController: UIViewController {
 
-  @IBAction func showPicker(_ sender: UIButton) {
-
-    guard let picker = ActionSheetStringPicker(title: "Tomato", rows: ["10 g", "20 g", "30 g", "40 g"], initialSelection: 0, doneBlock: { (picker, index, values) in
-      print("picker: \(String(describing: picker))")
-      print("index: \(index)")
-      print("values: \(String(describing: values))")
-    }, cancel: { ActionStringDoneBlock in return}, origin: sender) else { return }
-
-    picker.show()
-  }
+  var picker: ActionSheetStringPicker?
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  private func showPicker() {
+    guard let picker = ActionSheetStringPicker(title: "Tomato", rows: ["10 g", "20 g", "30 g", "40 g", "50 g", "60 g", "70 g", "80 g", "90 g", "100 g", "200 g"], initialSelection: 0, doneBlock: { (picker, index, values) in
+      print("picker: \(String(describing: picker))")
+      print("index: \(index)")
+      print("values: \(String(describing: values))")
+    }, cancel: { ActionStringDoneBlock in return}, origin: self.view) else { return }
+
+    picker.show()
+    self.picker = picker
+  }
+
+  private func addSuggestions(values: [String]) {
+    guard let bgView = self.picker?.actionSheet.bgView else {
+      print("Call .show() first: Suggestions can be added after picker is shown.")
+      return
+    }
+
+    var f = bgView.frame
+    let numOfButtons = CGFloat(values.count)
+    let pad: CGFloat = 10.0
+    let totalWidth = f.width
+    let totalHeight = CGFloat(40)
+    let buttonWidth = (totalWidth - (pad * (numOfButtons + 1))) / numOfButtons
+    let buttonHeight = totalHeight
+
+    f.size.height += totalHeight
+    f.origin.y -= totalHeight
+    bgView.frame = f
+
+    for idx in 0..<values.count {
+
+      let title = values[idx]
+      let x = (CGFloat(idx) * (buttonWidth + pad)) + pad
+      let y = CGFloat(0)
+
+      let button = UIButton(frame: CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight))
+      button.setTitle(title, for: .normal)
+      button.setTitleColor(.red, for: .normal)
+      button.backgroundColor = .lightGray
+      button.addTarget(self, action: #selector(suggestionTapped(_ :)), for: .touchUpInside)
+
+      bgView.addSubview(button)
+    }
+  }
+
+  @IBAction func showPicker(_ sender: UIButton) {
+    showPicker()
+  }
+
+  @IBAction func showSuggestionsPicker(_ sender: UIButton) {
+    showPicker()
+    addSuggestions(values: ["10 g", "20 g", "50 g", "200 g"])
+  }
+
+  @objc func suggestionTapped(_ sender: UIButton) {
+    print("tapped")
   }
 }
 
