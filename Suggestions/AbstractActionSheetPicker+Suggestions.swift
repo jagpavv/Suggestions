040456c036d6d399878755578fb8b8545480456c
Eunjin Kim <jagpavv@gmail.com>
2019-01-14 13:19:28 +0100

    Beautify suggestions view

--- Suggestions/AbstractActionSheetPicker+Suggestions.swift
+++ Suggestions/AbstractActionSheetPicker+Suggestions.swift
@@ -31,33 +31,34 @@ extension AbstractActionSheetPicker {
 
     var f = bgView.frame
     let numOfButtons = CGFloat(values.count)
-    let pad: CGFloat = 10.0
+    let pad: CGFloat = 1.0
     let totalWidth = f.width
     let totalHeight = CGFloat(40)
-    let buttonWidth = (totalWidth - (pad * (numOfButtons + 1))) / numOfButtons
+    let buttonWidth = (totalWidth - (pad * (numOfButtons - 1))) / numOfButtons
     let buttonHeight = totalHeight
 
     f.size.height += totalHeight
     f.origin.y -= totalHeight
     bgView.frame = f
 
-    for idx in 0..<values.count {
+    let view = UIView(frame: CGRect(x: 0, y: 0, width: totalWidth, height: totalHeight))
+    view.backgroundColor = .white
 
-      let title = values[idx]
-      let x = (CGFloat(idx) * (buttonWidth + pad)) + pad
+    values.enumerated().forEach { index, value in
+      let x = CGFloat(index) * (buttonWidth + pad)
       let y = CGFloat(0)
 
       let button = SuggestionButton(frame: CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight))
-      button.setTitle(title, for: .normal)
-      button.setTitleColor(.red, for: .normal)
-      button.backgroundColor = .lightGray
+      button.setTitle(value, for: .normal)
+      button.setTitleColor(.white, for: .normal)
+      button.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
       button.suggestionSelected {
-        doneBlock(self, idx, title)
+        doneBlock(self, index, value)
         self.hideWithCancelAction()
       }
-
-      bgView.addSubview(button)
+      view.addSubview(button)
     }
 
+    bgView.addSubview(view)
   }
 }
